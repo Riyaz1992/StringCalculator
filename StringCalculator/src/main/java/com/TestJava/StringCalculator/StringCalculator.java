3599603b4a3c57300755e54538c3b781fa78d914
gamerockz <riyaza42874@gmail.com>
2020-07-23 09:44:33 +0530

    Single value Test

--- StringCalculator/src/main/java/com/TestJava/StringCalculator/StringCalculator.java
+++ StringCalculator/src/main/java/com/TestJava/StringCalculator/StringCalculator.java
@@ -8,6 +8,10 @@ public class StringCalculator
 {
      public int Add(String numbers)
      {
-    	 return 0;
+    	 if(numbers.isEmpty())
+    	 {
+    		return 0;
+    	 }
+    	 return Integer.parseInt(numbers);
      }
 }
--- StringCalculator/src/test/java/com/TestJava/StringCalculator/StringCalculatorTest.java
+++ StringCalculator/src/test/java/com/TestJava/StringCalculator/StringCalculatorTest.java
@@ -2,15 +2,25 @@ package com.TestJava.StringCalculator;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 /**
  * Unit test for simple App.
  */
 public class StringCalculatorTest 
 {
+	StringCalculator calculator;
+	@Before
+	public void init() {
+		calculator = new StringCalculator();	
+	}
 	@Test
 	public void emptyString()
 	{
-		StringCalculator calculator = new StringCalculator();
 		assertEquals(calculator.Add(""),0);
+	}
+	@Test
+	public void singleValue() {
+	   assertEquals(calculator.Add("1"),1);	
 	}
 }
